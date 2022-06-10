3e3e61f8f2bff748f1a8ad28748ecc52f3e6dfab
MateoMazziniOngay <mateomazzini610@gmail.com>
2022-06-10 20:26:16 -0300

    Modificado formato fecha en ventana visitas

--- src/interfaz/ventanaVisitas.java
+++ src/interfaz/ventanaVisitas.java
@@ -23,11 +23,7 @@ public class ventanaVisitas extends javax.swing.JFrame {
         comboClientes = new javax.swing.JComboBox<>();
         comboDepositos = new javax.swing.JComboBox<>();
         comboEmpleados = new javax.swing.JComboBox<>();
-        inputDia = new javax.swing.JTextField();
-        inputMes = new javax.swing.JTextField();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
-        inputAnio = new javax.swing.JTextField();
+        inputFecha = new javax.swing.JFormattedTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
@@ -56,20 +52,8 @@ public class ventanaVisitas extends javax.swing.JFrame {
 
         comboEmpleados.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
-        inputDia.setText("DD");
-
-        inputMes.setText("MM");
-
-        jLabel6.setText("/");
-
-        jLabel7.setText("/");
-
-        inputAnio.setText("AA");
-        inputAnio.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                inputAnioActionPerformed(evt);
-            }
-        });
+        inputFecha.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.DateFormatter(java.text.DateFormat.getDateInstance(java.text.DateFormat.SHORT))));
+        inputFecha.setText("DD/MM/AA");
 
         javax.swing.GroupLayout pnlPanelLayout = new javax.swing.GroupLayout(pnlPanel);
         pnlPanel.setLayout(pnlPanelLayout);
@@ -91,16 +75,7 @@ public class ventanaVisitas extends javax.swing.JFrame {
                         .addComponent(comboClientes, 0, 199, Short.MAX_VALUE)
                         .addComponent(comboDepositos, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(comboEmpleados, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addGroup(pnlPanelLayout.createSequentialGroup()
-                        .addComponent(inputDia, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel7)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(inputMes, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(1, 1, 1)
-                        .addComponent(inputAnio, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addComponent(inputFecha, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(90, Short.MAX_VALUE))
         );
         pnlPanelLayout.setVerticalGroup(
@@ -123,11 +98,7 @@ public class ventanaVisitas extends javax.swing.JFrame {
                 .addGap(18, 18, 18)
                 .addGroup(pnlPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4)
-                    .addComponent(inputDia, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(inputMes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel6)
-                    .addComponent(jLabel7)
-                    .addComponent(inputAnio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(inputFecha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 17, Short.MAX_VALUE)
                 .addComponent(btnAtras, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(44, 44, 44))
@@ -145,10 +116,6 @@ public class ventanaVisitas extends javax.swing.JFrame {
         this.setVisible(false);
     }//GEN-LAST:event_btnAtrasActionPerformed
 
-    private void inputAnioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_inputAnioActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_inputAnioActionPerformed
-
     public static void main(String args[]) {
 
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -163,15 +130,11 @@ public class ventanaVisitas extends javax.swing.JFrame {
     private javax.swing.JComboBox<String> comboClientes;
     private javax.swing.JComboBox<String> comboDepositos;
     private javax.swing.JComboBox<String> comboEmpleados;
-    private javax.swing.JTextField inputAnio;
-    private javax.swing.JTextField inputDia;
-    private javax.swing.JTextField inputMes;
+    private javax.swing.JFormattedTextField inputFecha;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel lblVisitas;
     private javax.swing.JPanel pnlPanel;
     // End of variables declaration//GEN-END:variables
