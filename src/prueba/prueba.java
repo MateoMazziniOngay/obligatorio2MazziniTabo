ca3535040be896a1861ec05b4f2d74552d142de8
Martin Tabó <martintabo26@gmail.com>
2022-06-11 11:03:51 -0300

    clase prueba

---
+++ src/prueba/prueba.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prueba;
+ import interfaz.*;
+/**
+ *
+ * @author tinti
+ */
+public class prueba {
+    
+    static ventanaInicio vent = new ventanaInicio();
+    public static void main(final String[] args) {
+        vent.setVisible(true);
+    }
+}
